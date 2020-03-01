4070b8d8d8049b1b96ba395d839f907468b899df
Julio Berina <julioberina@outlook.com>
2020-03-01 01:40:26 -0800

    First commit

---
+++ BrainfuckInterpreter/Interpreter.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BrainfuckInterpreter
+{
+    public class Interpreter
+    {
+        private List<int> _cells;
+        private Stack<int> _stack;
+        private int _index;
+
+        public Interpreter()
+        {
+            _cells = new List<int>() { 0 };
+            _stack = new Stack<int>();
+            _index = 0;
+        }
+
+        public void Parse(string code)
+        {
+            int ci = 0;
+            char cmd = ' ';
+
+            while (ci < code.Length)
+            {
+                cmd = code[ci];
+
+                switch (cmd)
+                {
+                    case '+': _cells[_index] += 1; ++ci; break;
+                    case '-': _cells[_index] -= 1; ++ci; break;
+                    case '<': ShiftLeft(); ++ci; break;
+                    case '>': ShiftRight(); ++ci; break;
+                    case '[': StartLoop(code, ref ci); ++ci; break;
+                    case ']': EndOrRestartLoop(ref ci); break;
+                    case '.': Console.Write((char)_cells[_index]); ++ci; break;
+                }
+            }
+        }
+
+        private void ShiftLeft()
+        {
+            if (_index == 0)
+                _cells.Insert(0, 0);
+            else
+                --_index;
+        }
+
+        private void ShiftRight()
+        {
+            if (_index + 1 == _cells.Count)
+            {
+                _cells.Add(0);
+                ++_index;
+            }
+            else
+                _index += 1;
+        }
+
+        private void StartLoop(string code, ref int ci)
+        {
+            if (_cells[_index] == 0) 
+                while (code[ci] != ']') ci += 1;
+
+            else _stack.Push(ci);
+        }
+
+        private void EndOrRestartLoop(ref int ci)
+        {
+            ci = ((_cells[_index] == 0) ? ci + 1 : _stack.Pop());
+        }
+    }
+}
---
+++ BrainfuckInterpreter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BrainfuckInterpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Interpreter ip = new Interpreter();
+            string code = "++++++[>++++++++++<-]>+++++.+.+.+.";
+
+            ip.Parse(code);
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
